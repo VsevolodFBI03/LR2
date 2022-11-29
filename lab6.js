9f2bec6e8c30f1d1d5a43e79ed79fda788287973
Шениязов Всеволод <kosheniyazow@gmail.com>
2022-11-29 11:04:15 +0700

    Alert

--- lab6.js
+++ lab6.js
@@ -8,14 +8,16 @@ function setRussian() {
   document.getElementById("your-name").innerHTML = "Ваше имя";
   document.getElementById("input-name").placeholder = "Введите имя";
   document.getElementById("your-surname").innerHTML = "Ваша фамилия";
-  document.getElementById("your-age").placeholder = "Введите ваш возраст";
+  document.getElementById("input-surname").placeholder = "Введите вашу фамилию";
+  document.getElementById("your-age").innerHTML = "Ваш возраст";
+  document.getElementById("input-age").placeholder = "Введите ваш возраст";
   document.getElementById("OK-button").value = "Кнопка ОК";
-  document.getElementById("Reset-button").value = "Кнопка СБРОС";
+  document.getElementById("RESET-button").value = "Кнопка СБРОС";
 }
 
 function setEnglish() {
   let name = document.getElementById("student");
-  name.innerHTML = "Всеволод Шениязов, Егор Коломеец";
+  name.innerHTML = "Vsevolod Sheniyazow, Egor Kolomeets";
 
   let footer = document.getElementById("footer");
   footer.innerHTML = "Faculty of Business, FBI-03, 3rd year, 2022";
@@ -23,7 +25,23 @@ function setEnglish() {
   document.getElementById("your-name").innerHTML = "Your name";
   document.getElementById("input-name").placeholder = "Enter your name";
   document.getElementById("your-surname").innerHTML = "Your surname";
-  document.getElementById("your-age").placeholder = "Enter your age";
+  document.getElementById("input-surname").placeholder = "Enter your surname";
+  document.getElementById("your-age").innerHTML = "Your age";
+  document.getElementById("input-age").placeholder = "Enter your age";
   document.getElementById("OK-button").value = "OK button";
-  document.getElementById("Reset-button").value = "Reset button";
+  document.getElementById("RESET-button").value = "Reset button";
+}
+
+function showAlert() {
+  let userName = document.getElementById("input-name").value;
+  let userSurname = document.getElementById("input-surname").value;
+  let userAge = document.getElementById("input-age").value;
+  alert(
+    "Привет, " + userName + " " + userSurname + "\n" + "Ваш возраст: " + userAge
+  );
+  let r = Math.round(Math.random() * 255);
+  let g = Math.round(Math.random() * 255);
+  let b = Math.round(Math.random() * 255);
+  let color = "rgb(" + r + "," + g + "," + b + ")";
+  document.getElementById("user-data").style.backgroundColor = color;
 }
