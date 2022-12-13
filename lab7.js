529caaaa7deafd47d0d1f64f7c8eee6411c71814
Шениязов Всеволод <kosheniyazow@gmail.com>
2022-12-13 09:50:10 +0700

    Лесенка

--- lab7.js
+++ lab7.js
@@ -1,33 +1,48 @@
 function showAlert() {
-    let userName = document.getElementById("input-name").value;
-    let userSurname = document.getElementById("input-surname").value;
-    let userAge = document.getElementById("input-age").value;
-    let userSex = document.getElementById('sex').value;
+  let userName = document.getElementById("input-name").value;
+  let userSurname = document.getElementById("input-surname").value;
+  let userAge = document.getElementById("input-age").value;
+  let userSex = document.getElementById("sex").value;
 
-    let greeting;
-    if(userAge <= 17) {
-    alert(
-      greeting = "Здравствуй, ";
-      if(userSex== 'female'){
-        greeting+= 'девочка';
-      }
-      else {
-        greeting == 'мальчик';
-      }
+  let greeting;
+  if (userAge <= 17) {
+    greeting = "Здравствуй, ";
+    if (userSex == "female") {
+      greeting += "девочка";
+    } else {
+      greeting += "мальчик";
     }
-    else {
-        greeting = 'Здравствуйте, ';
-        if(userSex== 'female'){
-            greeting+= 'госпожа';
+  } else {
+    greeting = "Здравствуйте, ";
+    if (userSex == "female") {
+      greeting += "госпожа";
+    } else {
+      greeting += "господин";
     }
-    else {
-        greeting += 'господин';
-    }
-        alert(greeting + userName + ' ' + userSurname + '\n' + 'Ваш возраст: ' + userAge);
-    
-    let r = Math.round(Math.random() * 255);
-    let g = Math.round(Math.random() * 255);
-    let b = Math.round(Math.random() * 255);
-    let color = "rgb(" + r + "," + g + "," + b + ")";
-    document.getElementById("user-data").style.backgroundColor = color;
   }
+  alert(
+    greeting + userName + " " + userSurname + "\n" + "Ваш возраст: " + userAge
+  );
+
+  let r = Math.round(Math.random() * 255);
+  let g = Math.round(Math.random() * 255);
+  let b = Math.round(Math.random() * 255);
+  let color = "rgb(" + r + "," + g + "," + b + ")";
+  document.getElementById("user-data").style.backgroundColor = color;
+}
+
+function makeStairs() {
+
+    let stairs = document.querySelector('#stairs');
+    let stepCount = 20;
+    for(let i = 0; i<stepCount; i++) {
+        stairs.innerHTML += '<div class = "step"></div>';
+    }
+
+    let steps = Document.querySelectorAll('.step');
+    for(let i = 0;< steps.lengt; i++){
+        step[i].style.left = (30 +10*i) +'px'; 
+        step[i].style.left = (300 +80*i) +'px'; 
+        steps[i].innerHTML = 'Блок номер' + i;
+    }
+}
