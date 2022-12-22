add8d1ad4f21fd4d13cf7037752a428e5a5595e9
Шениязов Всеволод <kosheniyazow@gmail.com>
2022-12-22 17:27:11 +0700

    Количество дней с даты рождения

--- lab8.js
+++ lab8.js
@@ -4,3 +4,13 @@
     out.innerHTML = today.toLocaleDateString();
   }  
   window.addEventListener('load',showDate);
+
+  function showDaysCount(params){
+    let today = new Date();
+    let inputDate = document.querySelector('input[type=date]');
+    let birthday = new Date(inputDate.value);
+    let daysCount = (today - birthday)/1000/60/60/24 ;
+    let result = document.getElementById('result');
+    daysCount = Math.floor(daysCount);
+    result.innerHTML = 'С даты рождения прошло: '+ daysCount;
+  }
